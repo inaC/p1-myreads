f64e069b216d9230b218860159093405eed1a6b4
Inacio <inaciogcerqueira@gmail.com>
2017-12-14 22:02:42 -0200

    feature: Create Shelf component

---
+++ src/Shelf.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Book from './Book'
+import PropTypes from 'prop-types'
+
+class Shelf extends React.Component {
+	static propTypes = {
+		name: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		books: PropTypes.array.isRequired,
+		onMoveToShelf: PropTypes.func.isRequired
+	}
+
+	render() {
+		const name = this.props.name
+		const title = this.props.title
+		const books = this.props.books
+		const onMoveToShelf = this.props.onMoveToShelf
+
+		return (
+			<div className="bookshelf" key={name}>
+		  <h2 className="bookshelf-title"> {title} </h2>
+		  <div className="bookshelf-books">
+		  	<ol className="books-grid">
+		  		{books.map((book) => (
+		  			<Book key={book.id} book={book} onSelect={onMoveToShelf}/>
+		  		))}
+		  	</ol>
+		  </div>
+		</div>
+		)
+	}
+}
+
+export default Shelf
